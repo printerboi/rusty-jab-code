9c8f4b25a1e3c2d4638fb86f97ba8acd5e073b1d
Jako9 <cubecrafter.lpt@gmail.com>
2021-10-03 00:29:04 +0200

    Implementing png export via image_maker using 2d Vectors

--- src/utils/image_maker.rs
+++ src/utils/image_maker.rs
@@ -27,6 +27,18 @@ fn attempt_to_create_folder() -> bool{
     };
 }
 
-pub fn color_pixel(buffer: &mut image::RgbImage, x: u32, y: u32, color: u8){
-    buffer.put_pixel(x, y, image::Rgb([255, 255, 255]));
+fn color_pixel(buffer: &mut image::RgbImage, x: u32, y: u32, color: Color){
+    let mut pixel= image::Rgb(rgb_of_color(color));
+    buffer.put_pixel(x, y, pixel);
+}
+
+pub fn render_to_png(image:Vec<Vec<Color>>) -> (){
+    let mut buffer = create_image_buffer(image.len() as u32,image[0].len() as u32);
+    for i in 0..image.len(){
+        for j in 0..image[i].len(){
+            color_pixel(&mut buffer,i as u32,j as u32,image[i][j]);
+        }
+    }
+
+    save_image_buffer(buffer);
 }
