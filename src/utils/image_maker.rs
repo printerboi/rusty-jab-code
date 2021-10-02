5fdae058d67cc2ed21b6b1b3901b4cbc5f12c338
Maximilian Krebs <maximilian-krebs@online.de>
2021-10-03 00:18:32 +0200

    handled dynamic creation of the render folder

--- src/utils/image_maker.rs
+++ src/utils/image_maker.rs
@@ -1,11 +1,29 @@
 extern crate image;
+use std::fs;
 
 pub fn create_image_buffer(imgx: u32, imgy: u32) -> image::RgbImage{
     return image::ImageBuffer::new(imgx, imgy);
 }
 
-pub fn save_image_buffer(buffer: image::RgbImage) {
-    buffer.save("render/code.png");
+pub fn save_image_buffer(buffer: image::RgbImage) -> bool {
+    attempt_to_create_folder();
+    let attempt = buffer.save("render/code.png");
+
+    //Error-handling needs to be refactored later
+    match attempt {
+        Ok(()) => return true,
+        Err(error) => return false,
+    };
+}
+
+fn attempt_to_create_folder() -> bool{
+    let attempt = fs::create_dir("render");
+    
+    //Error-handling needs to be refactored later
+    match attempt {
+        Ok(()) => return true,
+        Err(error) => return false,
+    };
 }
 
 pub fn color_pixel(buffer: &mut image::RgbImage, x: u32, y: u32, color: u8){
